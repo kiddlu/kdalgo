ce7c63c3d6afd63dafe78490c918bc652a3d0ccf
kiddlu <kidd.dawny.lu@gmail.com>
2018-08-13 16:14:44 +0800

    add queue array implememt
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ c/data-struct/queue/fifo/array/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main( )
+{
+  Queue queue;
+  Queue *q = &queue;
+  int i;
+
+  init(q);
+  for( i = 0; i < 24; i++ )
+    enqueue(q, 100 - i);
+
+  while(!empty(q)) {
+    printf( "%d ", front(q));
+    dequeue(q);
+  }
+
+  final(q);
+
+  return 0;
+}
